4009ff13097cd5c3a2b2e4ecd8fe25c3170178ff
Lev Alyukov <lev.alyukov@gmail.com>
2024-07-04 16:29:39 +0500

    v0.4

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 ## CalculatorApp
-**CalculatorApp** — калькулятор, который позволяет выполнять математические вычисления.
+**CalculatorApp** — калькулятор, который позволяет выполнять простые математические вычисления.
 
 Автор только начал изучать C++ и был бы признателен за любой совет или помощь в разработке.
 
-![screenshot_1](https://sun9-23.userapi.com/impg/Q4m7wr0Hei96VJx3yAEkrSDrZtZPWR-WNGMCTw/8LDKiNy1Sxs.jpg?size=705x216&quality=95&sign=4b54b5db501d231e51575d51fc61c3db&type=album)
+![screenshot_1](https://sun9-31.userapi.com/impg/QZphC-iKGhgUJUwedKZ2Io2-QAVY4OwP2ZDPjg/G4-VQd-JaBQ.jpg?size=790x166&quality=95&sign=c6e0cd7b985c7eb67e8be667752e77cb&type=album)
 
 ## Контакты
 * Telegram: [https://t.me/levalyukovtelegram](https://t.me/levalyukovtelegram)
 * Twitter: [https://x.com/levalyukov](https://x.com/levalyukov)
 * Почта: lev.alyukov@gmail.com
 
-## License
+## Лицензия
 Репозиторий имеет [MIT](LICENSE) лицензию.
--- main.cpp
+++ main.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
 #include <iomanip>
+#include <sstream>
+#include <cctype>
 #include <string>
 #include <map>
 
 std::string name = "CalculatorApp ";
-std::string version = "v0.3 \n";
+std::string version = "v0.4 \n";
 std::string formuls = "Available mathematical operators: +, -, *, / \n";
 
 void welcome() {std::cout << name << version << formuls << std::endl;}
 
+bool isInteger(const std::string& str) {
+    try {
+        std::size_t pos;
+        std::stoi(str, &pos);
+        return pos == str.length();
+    } 
+	catch (const std::invalid_argument& e) {return false;} 
+	catch (const std::out_of_range& e) {return false;}
+}
+
+bool isDouble(const std::string& str) {
+    try {
+        std::size_t pos;
+        std::stod(str, &pos);
+        return pos == str.length();
+	} 
+	catch (const std::invalid_argument& e) {return false;} 
+	catch (const std::out_of_range& e) {return false;}
+}
+
 int main() {
 	welcome();
 
-	calculator: 
+	app: 
 		char mathOperator;
 		double x,y,result;
 
@@ -21,13 +43,22 @@ int main() {
 		std::string c = std::to_string(y);
 		std::string d = std::to_string(result);
 
-		std::cout << "Input x: ";
+		std::cout << "Enter first number: ";
 		std::getline(std::cin, a);
 		std::cout << "Operator: ";
 		std::getline(std::cin, b);
-		std::cout << "Input y: ";
+		std::cout << "Enter second number: ";
 		std::getline(std::cin, c);
 
+		if (isInteger(a)) {goto calculation;} 
+		else if (isDouble(a)) {goto calculation;} 
+		else {std::cout << "Error: X is a string.\n" << std::endl; goto app;}
+
+		if (isInteger(c)) {goto calculation;} 
+		else if (isDouble(c)) {goto calculation;} 
+		else {std::cout << "Error: Y is a string.\n" << std::endl; goto app;}
+
+	calculation:
 		mathOperator = b.at(0);
 
 		switch (mathOperator) {
@@ -52,17 +83,14 @@ int main() {
 				break;
 
 			default:
-				std::cout << "Error: Invalid mathematical operator." << std::endl;
-				std::cout << "------" << std::endl;
-				goto calculator;
+				std::cout << "Error: Invalid mathematical operator.\n" << std::endl;
+				goto app;
 				break;
 		}
 
 	result:
-		std::cout << std::fixed << "Result: " << stod(a) << " " << mathOperator << " " << stod(c) << " = " << result << std::endl;
-		std::cout << "------" << std::endl;
-
-		goto calculator;
+		std::cout << std::fixed << "Result: " << stod(a) << " " << mathOperator << " " << stod(c) << " = " << result << "\n" << std::endl;
+		goto app;
 
 	return 0;
 }
