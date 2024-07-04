b526235861fbf1a64dc4158dc78f39d909b05996
Lev Alyukov <lev.alyukov@gmail.com>
2024-07-04 15:11:23 +0500

    v0.3

--- main.cpp
+++ main.cpp
@@ -4,48 +4,65 @@
 #include <map>
 
 std::string name = "CalculatorApp ";
-std::string version = "v0.2\n";
+std::string version = "v0.3 \n";
+std::string formuls = "Available mathematical operators: +, -, *, / \n";
 
-void welcome() {std::cout << name << version << std::endl;}
+void welcome() {std::cout << name << version << formuls << std::endl;}
 
 int main() {
-	double x;
-	double y;
-	double result;
+	welcome();
 
-	char mathOperator;
-	
-	std::string a = std::to_string(x);
-	std::string b = std::to_string(mathOperator);
-	std::string c = std::to_string(y);
-	std::string d = std::to_string(result);
+	calculator: 
+		char mathOperator;
+		double x,y,result;
 
-	std::cout << "Input: ";
-	std::getline(std::cin, a);
-	std::cout << "Operator: ";
-	std::getline(std::cin, b);
-	std::cout << "Input: ";
-	std::getline(std::cin, c);
+		std::string a = std::to_string(x);
+		std::string b = std::to_string(mathOperator);
+		std::string c = std::to_string(y);
+		std::string d = std::to_string(result);
 
-	mathOperator = b.at(0);
-	
-	switch (mathOperator) {
-		case '+':
-			result = stod(a) + stod(c);
-			break;
-		case '-':
-			result = stod(a) - stod(c);
-			break;
-		case '*':
-			result = stod(a) * stod(c);
-			break;
-		case '/':
-			result = stod(a) / stod(c);
-			break;
-		default:
-			std::cout << "Error: Invalid mathematical operator.";
-			break;
-	}
+		std::cout << "Input x: ";
+		std::getline(std::cin, a);
+		std::cout << "Operator: ";
+		std::getline(std::cin, b);
+		std::cout << "Input y: ";
+		std::getline(std::cin, c);
 
-	std::cout << std::fixed << "Result: " << stod(a) << " " << mathOperator << " " << stod(c) << " = " << result << std::endl;
+		mathOperator = b.at(0);
+
+		switch (mathOperator) {
+			case '+':
+				result = stod(a) + stod(c);
+				goto result;
+				break;
+				
+			case '-':
+				result = stod(a) - stod(c);
+				goto result;
+				break;
+				
+			case '*':
+				result = stod(a) * stod(c);
+				goto result;
+				break;
+
+			case '/':
+				result = stod(a) / stod(c);
+				goto result;
+				break;
+
+			default:
+				std::cout << "Error: Invalid mathematical operator." << std::endl;
+				std::cout << "------" << std::endl;
+				goto calculator;
+				break;
+		}
+
+	result:
+		std::cout << std::fixed << "Result: " << stod(a) << " " << mathOperator << " " << stod(c) << " = " << result << std::endl;
+		std::cout << "------" << std::endl;
+
+		goto calculator;
+
+	return 0;
 }
