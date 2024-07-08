8733e4a96885062942ddcafee9908793c19486c1
Lev Alyukov <lev.alyukov@gmail.com>
2024-07-08 11:15:47 +0500

    v0.6
    - Add new math operators: sin, cos
    - Added support for commands
    - Fixed the error handler

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## CalculatorApp
 **CalculatorApp** — калькулятор, который позволяет выполнять простые математические вычисления.
 
-![screenshot_1](https://sun9-55.userapi.com/impg/_dqDijSEakUO9_TloRWS_fbaf1Y7ejgoH-fx4Q/pxt2pGX9ezU.jpg?size=651x175&quality=95&sign=6066d05573aaa3a751a36bcf6adb7ce3&type=album)
+![screenshot_1](https://sun9-12.userapi.com/impg/2ZAKJ0AHu1DSq4KZ9DrjrSI9DhTlWAgoHYlBmw/RX_s5x2wlB8.jpg?size=964x156&quality=95&sign=00bb6cfc77fc739f480055cfc12467f4&type=album)
--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
-#include <cctype>
 #include <string>
+#include <cmath>
 #include <map>
 
-std::string name = "\nCalculatorApp ";
-std::string version = "v0.5 \n";
-std::string formuls = "Available mathematical operators: +, -, *, / \n";
+#define PI 3.14159265
 
-void welcome() {std::cout << name << version << formuls << std::endl;}
+std::string name = "\nCalculatorApp ";
+std::string version = "v0.6";
+std::string availableOperators = "Available mathematical operators: '+', '-', '*', '/', 'sin', 'cos', | Commands: 'exit'\n";
+
+void welcome() {std::cout << name << version << std::endl;}
+void formuls() {std::cout << availableOperators << std::endl;}
 
 bool isInteger(const std::string& str) {
     try {
@@ -32,27 +35,33 @@ bool isDouble(const std::string& str) {
 }
 
 bool check(const std::string& a, const std::string& c) {
-	return (isInteger(a) && isDouble(a)) && (isInteger(c) && isDouble(c));
+	return (isInteger(a) || isDouble(a)) && (isInteger(c) || isDouble(c));
 }
 
 int main() {
 	welcome();
 
-	app: 
+	app:
+		formuls();
+
 		char mathOperator;
 		double x,y,result;
 
 		std::string a = std::to_string(x);
 		std::string b = std::to_string(mathOperator);
 		std::string c = std::to_string(y);
-		std::string d = std::to_string(result);
 
-		std::cout << "Enter first number: ";
-		std::getline(std::cin, a);
-		std::cout << "Enter math operator: ";
+		std::cout << "Enter math operator or command: ";
 		std::getline(std::cin, b);
-		std::cout << "Enter second number: ";
-		std::getline(std::cin, c);
+
+		if (b != "exit") {
+			if (b != "sin" && b != "cos") {
+				std::cout << "Enter first value: ";
+				std::getline(std::cin, a);
+				std::cout << "Enter second value: ";
+				std::getline(std::cin, c);
+			} else {std::cout << "Enter value: "; std::getline(std::cin, a); c = std::to_string(0);}
+		} else {goto exit_app;}
 
 		if (check(a,c)) {} 
 		else {std::cout << "Error: Invalid values.\n" << std::endl; goto app;}
@@ -61,24 +70,38 @@ int main() {
 		mathOperator = b.at(0);
 
 		switch (mathOperator) {
-			case '+':
+			case '+': // add
 				result = stod(a) + stod(c);
 				goto result;
 				break;
 				
-			case '-':
+			case '-': // subtract
 				result = stod(a) - stod(c);
 				goto result;
 				break;
 				
-			case '*':
+			case '*': // multiply
 				result = stod(a) * stod(c);
 				goto result;
 				break;
 
-			case '/':
+			case '/': // divide
 				result = stod(a) / stod(c);
 				goto result;
+				break;
+
+			case 's': // sinus
+				result = sin(stod(a)*PI/180);
+				goto result;
+				break;
+
+			case 'c': // cosinus
+				result = sin(stod(a)*PI/180);
+				goto result;
+				break;
+
+			case 'e': // exit the app
+				goto exit_app;
 				break;
 
 			default:
@@ -88,8 +111,16 @@ int main() {
 		}
 
 	result:
-		std::cout << std::fixed << "Result: " << stod(a) << " " << mathOperator << " " << stod(c) << " = " << result << "\n" << std::endl;
+		if (b == "sin") {
+			std::cout << std::fixed << "Result: " << "sin" << "(" << stod(a) <<")" << " = " << result << "\n" << std::endl;
+		} else if (b == "cos") {
+			std::cout << std::fixed << "Result: " << "cos" << "(" << stod(a) <<")" << " = " << result << "\n" << std::endl;
+		} else {
+			std::cout << std::fixed << "Result: " << stod(a) << " " << mathOperator << " " << stod(c) << " = " << result << "\n" << std::endl;
+		}
+		std::cout << "------" << std::endl;
 		goto app;
 
-	return 0;
+	exit_app:
+		return 0;
 }
